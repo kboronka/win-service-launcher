791154ad4e77aa13baad1a9c702291224217839e
kboronka@gmail.com <kboronka@gmail.com@954606c5-ee7c-c1ef-23bc-2798543850e6>
2013-05-26 15:20:06 +0000

    commit of demo tester

---
+++ trunk/demo_service/ProjectInstaller.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: kboronka
+ * Date: 2013-05-22
+ * Time: 10:30 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace demo_service
+{
+	[RunInstaller(true)]
+	public class ProjectInstaller : Installer
+	{
+		private ServiceProcessInstaller serviceProcessInstaller;
+		private ServiceInstaller serviceInstaller;
+		
+		public ProjectInstaller()
+		{
+			serviceProcessInstaller = new ServiceProcessInstaller();
+			serviceInstaller = new ServiceInstaller();
+			// Here you can set properties on serviceProcessInstaller or register event handlers
+			serviceProcessInstaller.Account = ServiceAccount.LocalService;
+			
+			serviceInstaller.ServiceName = demo_service.MyServiceName;
+			this.Installers.AddRange(new Installer[] { serviceProcessInstaller, serviceInstaller });
+		}
+	}
+}
