1539b8685f9ed5a8df6cf4c434b45fee6f5c13a3
kboronka@gmail.com <kboronka@gmail.com@954606c5-ee7c-c1ef-23bc-2798543850e6>
2013-06-23 17:29:15 +0000

    added a logger

---
+++ WinServiceLauncher/Launcher.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Boronka
+ * Date: 7/1/2013
+ * Time: 9:28 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using skylib.Tools;
+
+namespace WinServiceLauncher
+{
+	public class Launcher
+	{
+		private string filename;
+		private string arguments;
+		private string domain;
+		private string username;
+		private string password;
+		
+		public Launcher(string filename, string arguments)
+		{
+			this.filename = filename;
+			this.arguments = arguments;
+		}
+
+		public Launcher(string filename, string arguments, string username, string password)
+		{
+			this.filename = filename;
+			this.arguments = arguments;
+			this.domain = System.Environment.MachineName;
+			this.username = username;
+			this.password = password;
+		}
+		
+		public Launcher(string filename, string arguments, string domain, string username, string password)
+		{
+			this.filename = filename;
+			this.arguments = arguments;
+			this.domain = domain;
+			this.username = username;
+			this.password = password;
+		}
+		
+		public void Launch()
+		{
+			try
+			{
+				WinServiceLauncher.Log("Launching " + this.filename + this.arguments);
+				
+				if (String.IsNullOrEmpty(domain))
+				{
+					ConsoleHelper.Start(this.filename, this.arguments);
+				}
+				else
+				{
+					ConsoleHelper.StartAs(this.filename, this.arguments, this.domain, this.username, this.password);
+				}
+				WinServiceLauncher.Log("Launching complete");
+			}
+			catch (Exception ex)
+			{
+				WinServiceLauncher.Log("Launching failed");
+				WinServiceLauncher.Log(ex);
+			}
+		}
+	}
+}
--- WinServiceLauncherSetup/MainForm.cs
+++ WinServiceLauncherSetup/MainForm.cs
@@ -13,21 +13,11 @@ using System.Windows.Forms;
 
 namespace WinServiceLauncherSetup
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
 	public partial class MainForm : Form
 	{
 		public MainForm()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
 			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
 		}
 	}
 }
