1478a163eea29ba94b10cc5465ce47114f66e316
kboronka@gmail.com <kboronka@gmail.com@954606c5-ee7c-c1ef-23bc-2798543850e6>
2013-06-23 18:53:19 +0000

    added XML Serializer and Deserialize

--- WinServiceLauncher/Launcher.cs
+++ WinServiceLauncher/Launcher.cs
@@ -7,17 +7,25 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
 using sar.Tools;
 
 namespace WinServiceLauncher
 {
 	public class Launcher
 	{
+		private string name;
 		private string filename;
 		private string arguments;
 		private string domain;
 		private string username;
 		private string password;
+		
+		#region constructors
 		
 		public Launcher(string filename, string arguments)
 		{
@@ -43,6 +51,20 @@ namespace WinServiceLauncher
 			this.password = password;
 		}
 		
+		public Launcher(XmlReader reader)
+		{
+			this.name = reader.GetAttribute("name");
+			this.filename = reader.GetAttribute("filename");
+			this.arguments = reader.GetAttribute("arguments");
+			this.domain = reader.GetAttribute("domain");
+			this.username = reader.GetAttribute("username");
+			this.password = reader.GetAttribute("password");
+		}
+		
+		#endregion
+		
+		#region methods
+		
 		public void Launch()
 		{
 			try
@@ -65,5 +87,27 @@ namespace WinServiceLauncher
 				WinServiceLauncher.Log(ex);
 			}
 		}
+		
+		public void Serialize(XmlWriter writer)
+		{
+			if (String.IsNullOrEmpty(this.name)) this.name = "";
+			if (String.IsNullOrEmpty(this.filename)) this.filename = "";
+			if (String.IsNullOrEmpty(this.arguments)) this.arguments = "";
+			if (String.IsNullOrEmpty(this.domain)) this.domain = "";
+			if (String.IsNullOrEmpty(this.username)) this.username = "";
+			if (String.IsNullOrEmpty(this.password)) this.password = "";
+						
+			writer.WriteStartElement("Launcher");
+			writer.WriteAttributeString("name", this.name);
+			writer.WriteAttributeString("filename", this.filename);
+			writer.WriteAttributeString("arguments", this.arguments);
+			writer.WriteAttributeString("domain", this.domain);
+			writer.WriteAttributeString("username", this.username);
+			writer.WriteAttributeString("password", this.password);
+			writer.WriteEndElement();	// Launcher
+		}
+		
+		#endregion
+		
 	}
 }
