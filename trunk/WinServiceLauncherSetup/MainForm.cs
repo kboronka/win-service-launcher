43b07d129591dfa49444bef143d37180f95a3cb4
kboronka@gmail.com <kboronka@gmail.com@954606c5-ee7c-c1ef-23bc-2798543850e6>
2013-06-23 16:27:11 +0000

    added Setup app

---
+++ trunk/WinServiceLauncherSetup/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Boronka
+ * Date: 6/22/2013
+ * Time: 8:37 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinServiceLauncherSetup
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
