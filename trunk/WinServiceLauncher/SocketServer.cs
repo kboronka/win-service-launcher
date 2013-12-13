e8355ce02d5224ca3665d57064708aeaaadb4195
kboronka@gmail.com <kboronka@gmail.com@954606c5-ee7c-c1ef-23bc-2798543850e6>
2013-12-13 02:48:23 +0000

    SocketServer object

---
+++ trunk/WinServiceLauncher/SocketServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+using sar.Tools;
+
+namespace WinServiceLauncher
+{
+	public class SocketServer : sar.Socket.SocketServer
+	{
+	
+		#region constructors
+		
+		public SocketServer(XmlReader reader): base(int.Parse(reader.GetAttribute("port")), Encoding.ASCII)
+		{
+		
+		}
+		
+		#endregion
+		
+		#region methods
+				
+		public void Serialize(XmlWriter writer)
+		{
+			writer.WriteStartElement("SocketServer");
+			writer.WriteAttributeString("port", base.port.ToString());
+			writer.WriteEndElement();	// Launcher
+		}
+		
+		#endregion
+		
+	}
+}
