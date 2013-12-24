0a35139620113be66afd8d690531724d374f12c0
kboronka@gmail.com <kboronka@gmail.com@954606c5-ee7c-c1ef-23bc-2798543850e6>
2013-12-24 04:36:47 +0000

    version 0.0.0.14 release

---
+++ tags/0.0.0.14/WinServiceLauncher/WinLauncherService.cs
@@ -0,0 +1,125 @@
+/* Copyright (C) 2013 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+using sar.Tools;
+
+namespace WinServiceLauncher
+{
+	public class WinServiceLauncher : ServiceBase
+	{
+		public const string MyServiceName = "WinServiceLauncher";
+		
+		public WinServiceLauncher()
+		{
+			InitializeComponent();
+		}
+		
+		private void InitializeComponent()
+		{
+			this.ServiceName = MyServiceName;
+		}
+		
+		protected override void Dispose(bool disposing)
+		{
+			// TODO: Add cleanup code here (if required)
+			base.Dispose(disposing);
+		}
+		
+		protected override void OnStart(string[] args)
+		{
+			WinServiceLauncher.Log("OnStart");
+			Thread thread = new Thread(StartServices);
+			thread.Start();
+		}
+		
+		protected override void OnStop()
+		{
+			WinServiceLauncher.Log("OnStop");
+			// TODO: Add tear-down code here (if required) to stop your service.
+		}
+		
+		private void StartServices()
+		{
+			try
+			{
+				WinServiceLauncher.Log(ConsoleHelper.HR);
+				WinServiceLauncher.Log("StartServices()");
+				WinServiceLauncher.Log("Environment.UserInteractive = " + Environment.UserInteractive.ToString());
+				WinServiceLauncher.Log("Username = " + System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+				
+				Configuration.Load();
+				
+				foreach (Launcher app in Configuration.All.Launchers)
+				{
+					app.LaunchAsync();
+				}
+			}
+			catch (Exception ex)
+			{
+				WinServiceLauncher.Log(ex);
+			}
+		}
+		
+		public static void Log(Exception ex)
+		{
+			WinServiceLauncher.Log(ex.Message);
+			WinServiceLauncher.Log(ex.StackTrace);
+		}
+		
+		public static void Log(string message)
+		{
+			try
+			{
+				ServiceHelper.LogEvent(message);
+			}
+			catch
+			{
+				
+			}
+			
+			try
+			{
+				WinServiceLauncher.Logger.WriteLine(message);
+			}
+			catch
+			{
+				
+			}
+		}
+		
+		private static FileLogger logger;
+		public static FileLogger Logger
+		{
+			get
+			{
+				if (WinServiceLauncher.logger == null)
+				{
+					WinServiceLauncher.logger = new FileLogger("test.log");
+				}
+				
+				return WinServiceLauncher.logger;
+			}
+		}
+	}
+}
---
+++ tags/0.0.0.14/WinServiceLauncherTester/Program.cs
@@ -0,0 +1,89 @@
+/* Copyright (C) 2013 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using sar.Tools;
+
+namespace WinServiceLauncherTester
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{
+				ConsoleHelper.Start();
+				ConsoleHelper.ApplicationTitle();
+				
+				ConsoleHelper.WriteLine("Environment.UserInteractive = " + Environment.UserInteractive.ToString());
+				ConsoleHelper.WriteLine("Username = " + System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+
+				//return;
+				ConsoleHelper.KillProcess("PlexDlnaServer.exe");
+				ConsoleHelper.KillProcess("PlexScriptHost.exe");
+				ConsoleHelper.KillProcess("Plex Media Server.exe");
+				ConsoleHelper.KillProcess("PlexDlnaServer.exe");
+
+				// rename service exe file
+				string serviceEXE = IO.FindFile("WinServiceLauncher.exe");
+				string serviceFilename = IO.GetFilename(serviceEXE);
+				string serviceName = StringHelper.TrimEnd(serviceFilename, IO.GetFileExtension(serviceEXE).Length + 1);
+				string serviceRoot = IO.GetRoot(serviceEXE);
+				
+				ServiceHelper.TryStop("demo_service");
+				ServiceHelper.TryUninstall("demo_service");
+				
+				ServiceHelper.TryStop(serviceEXE);
+				ServiceHelper.TryUninstall(serviceEXE);
+				
+				//string username = ConsoleHelper.Input("Username: ");
+				//string password = ConsoleHelper.Input("Password: ");
+				
+				ServiceHelper.Install("4.0", serviceEXE);
+				ConsoleHelper.WriteLine(serviceName + " installed");
+				
+				
+				ServiceHelper.Start(serviceEXE);
+				ConsoleHelper.WriteLine(serviceName + " started");
+				ConsoleHelper.Write("done - press any key to stop and uninstall the service", ConsoleColor.Yellow);
+				ConsoleHelper.ReadKey();
+				ConsoleHelper.WriteLine();
+				
+				
+				ServiceHelper.TryStop(serviceEXE);
+				ConsoleHelper.WriteLine(serviceName + " stopped");
+				
+				
+				ServiceHelper.Uninstall("4.0", serviceEXE);
+				ConsoleHelper.WriteLine(serviceName + " uninstalled");
+				ConsoleHelper.Write("done - press any key to exit", ConsoleColor.Yellow);
+				ConsoleHelper.ReadKey();
+				ConsoleHelper.WriteLine();
+			}
+			catch (Exception ex)
+			{
+				ServiceHelper.TryStop("WinServiceLauncher");
+				ServiceHelper.TryUninstall("WinServiceLauncher");
+				ConsoleHelper.WriteException(ex);
+				ConsoleHelper.ReadKey();
+			}
+
+			ConsoleHelper.Shutdown();
+		}
+	}
+}
