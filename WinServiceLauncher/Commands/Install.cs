b94f8f011c100358cb832cb257fce6130f4dc41c
kboronka@gmail.com <kboronka@gmail.com@954606c5-ee7c-c1ef-23bc-2798543850e6>
2014-01-03 01:15:40 +0000

    added sar command loop base added install, uninstall, run commands

---
+++ WinServiceLauncher/Commands/Install.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Threading;
+using System.Reflection;
+using System.ServiceProcess;
+
+using sar.Base;
+using sar.Tools;
+
+namespace WinServiceLauncher.Commands
+{
+	public class Install : sar.Base.Command
+	{
+		public Install(sar.Base.CommandHub parent) : base(parent, "Install",
+		                                                  new List<string> { "install", "i" },
+		                                                  @"-i",
+		                                                  new List<string> { "-i" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 1)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			Progress.Message = "Installing Service";
+			List<string> installArgs = new List<string>();
+			
+			
+			if (this.commandHub.NoWarning) installArgs.Add(@"/LogToConsole = false");
+			if (!this.commandHub.NoWarning) installArgs.Add(@"/LogToConsole = true");
+
+			installArgs.Add(Assembly.GetExecutingAssembly().Location);
+			
+			ManagedInstallerClass.InstallHelper(installArgs.ToArray());
+			
+			ConsoleHelper.WriteLine("");
+			ConsoleHelper.WriteLine("Install Complete", ConsoleColor.Yellow);
+
+			return ConsoleHelper.EXIT_OK;
+		}
+	}
+}
