d5823f3f44ce1e6332462f26c3dc17778535977c
kboronka@gmail.com <kboronka@gmail.com@954606c5-ee7c-c1ef-23bc-2798543850e6>
2014-01-08 03:39:34 +0000

    updated CommandHub

--- WinServiceLauncher/CommandHub.cs
+++ WinServiceLauncher/CommandHub.cs
@@ -25,21 +25,16 @@ namespace WinServiceLauncher
 {
 	public class CommandHub : sar.Base.CommandHub
 	{
-		private List<Command> allCommands;
 		public CommandHub() : base()
 		{
 			// load all command modules
-			this.allCommands = new List<Command>() {
-				new Help(this),
-				new Kill(this),
-				new AppShutdownWait(this),
-				new Delay(this),
-				new Install(this),
-				new Uninstall(this),
-				new Run(this),
-				new Start(this),
-				new Stop(this)
-			};
+			base.commandList.AddRange(new Command[] {
+			                          	new Install(this),
+			                          	new Uninstall(this),
+			                          	new Run(this),
+			                          	new Start(this),
+			                          	new Stop(this)
+			                          });
 		}
 	}
 }
