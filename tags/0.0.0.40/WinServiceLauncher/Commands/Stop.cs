21d9be247a46b6d8db0edb5ccc3334af7862c7d2
kboronka@gmail.com <kboronka@gmail.com@954606c5-ee7c-c1ef-23bc-2798543850e6>
2014-01-18 02:35:18 +0000

    version 0.0.0.40 release

---
+++ tags/0.0.0.40/WinServiceLauncher/Commands/Stop.cs
@@ -0,0 +1,62 @@
+/* Copyright (C) 2014 Kevin Boronka
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Threading;
+using System.Reflection;
+using System.ServiceProcess;
+
+using sar.Base;
+using sar.Tools;
+
+namespace WinServiceLauncher.Commands
+{
+	public class Stop : sar.Base.Command
+	{
+		public Stop(sar.Base.CommandHub parent) : base(parent, "Stop",
+		                                              new List<string> { "stop" },
+		                                              @"-stop",
+		                                              new List<string> { "-stop" })
+		{
+		}
+		
+		public override int Execute(string[] args)
+		{
+			// sanity check
+			if (args.Length != 1)
+			{
+				throw new ArgumentException("incorrect number of arguments");
+			}
+			
+			Progress.Message = "Stopping Service";
+			bool success = ServiceHelper.TryStop("WinServiceLauncher.exe");
+			
+			if (success)
+			{
+				ConsoleHelper.WriteLine("");
+				ConsoleHelper.WriteLine("Service Stopped", ConsoleColor.Yellow);
+				return ConsoleHelper.EXIT_OK;
+			}
+			else
+			{
+				ConsoleHelper.WriteLine("");
+				ConsoleHelper.WriteLine("Service Failed to Stop", ConsoleColor.Red);
+				return ConsoleHelper.EXIT_ERROR;
+			}
+		}
+	}
+}
